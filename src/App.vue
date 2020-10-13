c3bbde643f2d0f1d134d918efa8de02cfcc79c7a
jiahao12 <15523443792@163.com>
2020-10-13 17:38:22 +0800

    添加elementui组件

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <!-- <el-button type="primary">123</el-button> -->
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
